cdf2eb65d72bbe3141345b0bb240711a22a158d6
Abid Miah <ec20298@qmul.ac.uk>
2023-06-04 13:41:51 +0100

    image

--- mywebsite/settings.py
+++ mywebsite/settings.py
@@ -117,6 +117,8 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
+ROOT_URLCONF = 'mywebsite.urls'
+
 STATIC_URL = 'static/'
 
 STATIC_ROOT = os.path.join(BASE_DIR, 'static/')
